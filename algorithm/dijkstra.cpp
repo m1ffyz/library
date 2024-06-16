ea777e09340fca64b158a512a380f71a6c1de6b7
m1ffyz <109456813+m1ffyz@users.noreply.github.com>
2024-06-16 12:09:06 +0900

    Create dijkstra.cpp
    ダイクストラ法

---
+++ algorithm/dijkstra.cpp
@@ -0,0 +1,24 @@
+// 重み付きグラフ, 開始地点
+vector<long long> dijkstra(vector<vector<pair<int, long long>>> G, int s){
+    int N = G.size();
+    vector<long long> dist(N, 1e18);
+    vector<bool> decided(N, false);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> Q;
+    dist[s] = 0;
+    Q.push({dist[s], s});
+    while(!Q.empty()){
+        int pos = Q.top().second;
+        Q.pop();
+        if(decided[pos]) continue;
+        decided[pos] = true;
+        rep(i, G[pos].size()){
+            int next = G[pos][i].first;
+            long long cost = G[pos][i].second;
+            if(dist[next] > dist[pos] + cost){
+                dist[next] = dist[pos] + cost;
+                Q.push({dist[next], next});
+            }
+        }
+    }
+    return dist;
+}
