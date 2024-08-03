d8e03478bccbad813963c05d22a26feab212b3b5
m1ffyz <109456813+m1ffyz@users.noreply.github.com>
2024-08-03 16:26:50 +0900

    Update check_palindrome.cpp

--- string/check_palindrome.cpp
+++ string/check_palindrome.cpp
@@ -1,5 +1,19 @@
-bool check_palindrome(string S){
-    int N = S.size();
-    rep(i, N / 2) if(S[i] != S[N - 1 - i]) return false;
+bool check_palindrome(string &S){
+    for(int i = 0; i < S.size() / 2; i ++){
+        if(S[i] != S[S.size() - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+// p: 開始位置,  l: 長さ
+bool check_palindrome(string &S, int p, int l){
+    if(p + l - 1 >= S.size()) return false;
+    for(int i = 0; i < l / 2; i ++){
+        if(S[p + i] != S[p + l - 1 - i]){
+            return false;
+        }
+    }
     return true;
 }
